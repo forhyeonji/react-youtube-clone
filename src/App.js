716b3fbb0d470bcbf7430c3aa6e16032c6e2ac5c
forhyeonji <ghkdguswl3@gmail.com>
2023-03-20 22:35:50 +0900

    CSS Header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SearchHeader from './components/SearchHeader';
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <SearchHeader />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/VideoDetail.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function VideoDetail() {
+  return <div>비데오 디테일</div>;
+}
