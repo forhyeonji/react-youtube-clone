f042d1ecd63490ce591056d93da8a08dfdee662c
forhyeonji <ghkdguswl3@gmail.com>
2023-06-29 23:05:00 +0900

    연관된 비디오 목록 test

--- src/components/test/ChannelInfo.test.js
+++ src/components/test/ChannelInfo.test.js
@@ -11,6 +11,22 @@ describe('ChannelInfo', () => {
 
   it('renders correctly', async () => {
     fakeYoutube.channelImageURL.mockImplementation(() => 'url');
+    const { asFragment } = render(
+      withAllContexts(
+        withRouter(
+          <Route path="/" element={<ChannelInfo id="id" name="channel" />} />
+        ),
+        fakeYoutube
+      )
+    );
+    await screen.findByRole('img');
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders without URL', () => {
+    fakeYoutube.channelImageURL.mockImplementation(() => {
+      throw new Error('error');
+    });
     render(
       withAllContexts(
         withRouter(
@@ -19,6 +35,21 @@ describe('ChannelInfo', () => {
         fakeYoutube
       )
     );
-    await waitFor(() => screen.findByText('channel'));
+
+    expect(screen.queryByRole('img')).toBeNull();
+  });
+
+  it('renders with URL', async () => {
+    fakeYoutube.channelImageURL.mockImplementation(() => 'url');
+    render(
+      withAllContexts(
+        withRouter(
+          <Route path="/" element={<ChannelInfo id="id" name="channel" />} />
+        ),
+        fakeYoutube
+      )
+    );
+
+    await screen.findByRole('img');
   });
 });
